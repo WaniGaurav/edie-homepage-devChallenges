d75db6d1ec6d6cf81f27e5c3dae234dedd8a3e08
WaniGaurav <gnwani95@gmail.com>
2023-09-07 21:17:44 +0530

    2nd commit

---
+++ components/navbar.js
@@ -0,0 +1,8 @@
+let displaySidebar = () =>{
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = 'flex';
+}
+let hideSidebar = () =>{
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = 'none';
+}
