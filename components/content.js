3835706a82c20398016983eed5f4ad8bc151e6d8
WaniGaurav <gnwani95@gmail.com>
2023-09-22 20:30:14 +0530

    14th commit

--- components/content.js
+++ components/content.js
@@ -10,6 +10,39 @@ document.documentElement.style.setProperty(
   
 // navbar scrolling padding end
 
+let btn = document.querySelector(".dark");
+btn.innerHTML="Change Mode";
+let changeMode = ()=> {
+    if (darkMode) {
+        darkMode()
+    }
+    else{
+        lightMode()
+    }
+}
+
+// Dark mode
+
+let darkMode = ()=>{
+    document.body.style.backgroundColor = "black";
+    document.documentElement.style.setProperty("--clr-primary","#eee",);
+    document.documentElement.style.setProperty("--clr-second","#eee");
+    document.documentElement.style.setProperty("--clr-third","#310089");
+}
+
+// Dark mode End
+
+// Light mode
+
+let lightMode = ()=>{
+    document.body.style.backgroundColor = "black";
+    document.documentElement.style.setProperty("--clr-primary","#333333");
+    document.documentElement.style.setProperty("--clr-second","#4F4F4F");
+    document.documentElement.style.setProperty("--clr-third","#828282");
+}
+
+// Light mode End
+
 let cardData = [
     {
         symbolsClass:'sharp',
